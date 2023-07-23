68bf935736cdd3f8613887286cdc3944356f8e55
joehobos <joehobos@DESKTOP-A520OBQ>
2023-07-23 00:23:33 -0700

    create card file

---
+++ Always-Coding/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PokemonList from "./PokemonList";
+import { useState } from "react";
+
+function Card() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Card
