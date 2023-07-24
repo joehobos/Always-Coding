fb828b7c723b6040cd981e927bc72bc6b1adbfdb
joehobos <joehobos@DESKTOP-A520OBQ>
2023-07-24 16:56:29 -0700

    final commit

--- Always-Coding/src/App.jsx
+++ Always-Coding/src/App.jsx
@@ -1,50 +1,93 @@
-import './App.css'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import React from 'react'
 import PokemonList from './components/PokemonList'
 import axios from 'axios'
 import Page from './components/Page'
+import './index.css'
 
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [allPokemon, setAllPokemon] = useState([])
   const [currentPageUrl, setcurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
   const [previousPageUrl, setpreviousPageUrl] = useState()
   const [nextPageUrl, setnextPageUrl] = useState()
   const [loading, setLoading] = useState(true)
-  
+  const [search, setSearch] = useState('')
+
   useEffect(() => {
-    setLoading(true)
-    axios.get(currentPageUrl).then(response => {
-      setLoading(false)
-      setPokemon (response.data.results.map(pokemon => pokemon.name))
+    setLoading(true);
+    axios.get(currentPageUrl).then((response) => {
+      setLoading(false);
+      const pokemonData = response.data.results.map((pokemon) => ({
+        name: pokemon.name,
+        url: pokemon.url,
+        weight: 0,
+        sprite: '', 
+      }));
+      setPokemon(pokemonData)
+      setAllPokemon(pokemonData)
       setpreviousPageUrl(response.data.previous)
       setnextPageUrl(response.data.next)
-    })
-  }, [currentPageUrl])
-  
-  function gotoPreviousPage (){
-    setcurrentPageUrl(previousPageUrl)
+
+      
+      Promise.all(
+        pokemonData.map(async (poke) => {
+          const response = await axios.get(poke.url);
+          const weight = response.data.weight;
+          const sprite = response.data.sprites.front_default; 
+          return { ...poke, weight, sprite };
+        })
+      ).then((updatedPokemon) => {
+        setPokemon(updatedPokemon);
+      });
+    });
+  }, [currentPageUrl]);
+
+  function gotoPreviousPage() {
+    setcurrentPageUrl(previousPageUrl);
   }
 
-  function gotoNextPage (){
-    setcurrentPageUrl(nextPageUrl)
+  function gotoNextPage() {
+    setcurrentPageUrl(nextPageUrl);
   }
 
-  if(loading === true) {
-    return "Loading..."
+  // Add this code at the end of your JavaScript file or inside a script tag in your HTML
+document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('enlarged');
+    });
+  });
+});
+
+
+  if (loading === true) {
+    return "Loading...";
   }
+
 
   return (
     <>
-      <PokemonList pokemon={pokemon} />
-      <Page 
-        gotoPreviousPage = {previousPageUrl ? gotoPreviousPage : null}
-        gotoNextPage = {nextPageUrl ? gotoNextPage : null} 
-      />
-    </>
-  )
+    <header className='header'>Pokedex</header>
+    <div className='search-container'>
+    <input
+      className='search'
+      type="text"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      placeholder="Search Pokemon"
+    />
+    </div>
+    <PokemonList pokemon={pokemon} search={search} />
+    <Page
+      gotoPreviousPage={previousPageUrl ? gotoPreviousPage : null}
+      gotoNextPage={nextPageUrl ? gotoNextPage : null}
+    />
+  </>
+  );
 }
 
 export default App;
--- Always-Coding/src/components/PokemonList.jsx
+++ Always-Coding/src/components/PokemonList.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import App from "../App";
+// import Search from "./Search";
 
 
 
-function PokemonList({ pokemon }) {
-   
-      return (
-        <div> 
-          {pokemon.map(p => (
-            <div key={p}>{p}</div>
-          ))}
+
+function PokemonList({ pokemon, search, }) {
+  const filteredPokemon = pokemon.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <>
+    <div className="cards">
+      {filteredPokemon.map((pokemon) => (
+        <div className="card" key={pokemon.name}>
+          <p>{pokemon.name}</p>
+          <img src={pokemon.sprite} alt='' />
+          <p> {pokemon.weight} kg</p>
+          
         </div>
-            
-        
-      )
-    
+      ))}
+    </div>
+    </>
+  );
 }
 
 export default PokemonList;
